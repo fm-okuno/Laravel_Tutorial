c2efba475e59dc48ff2dda07253cbf6849f663b5
gohta <okuno@freemake.co.jp>
2020-04-07 17:39:08 +0900

    BooksTableSeeder 　・シーダーファイルを作成。 　・runメソッド内にBooksテーブルに登録するためのレコードを記述 DatabaseSeeder.php 　・runメソッド内にBooksTableSeederを読み込む様にした sample/Book.php 　・DB操作を行う為のモデルを作成(app直下)。

--- sample/database/seeds/DatabaseSeeder.php
+++ sample/database/seeds/DatabaseSeeder.php
@@ -9,8 +9,9 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+     public function run()
+     {
+          // BooksTableSeederを読み込むように指定
+          $this->call(BooksTableSeeder::class);
+     }
 }
